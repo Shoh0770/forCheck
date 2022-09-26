0ecf56d6aa08701b2aa17ca871a990075175e4a4
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-09-26 17:38:52 +0500

    Initial Commit

---
+++ pushing1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pushing1
+//
+//  Created by Macbook Pro 14 Inch on 9/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pushing1/pushing1App.swift
@@ -0,0 +1,17 @@
+//
+//  pushing1App.swift
+//  pushing1
+//
+//  Created by Macbook Pro 14 Inch on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct pushing1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
